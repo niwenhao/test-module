e4e233193d2feb11cdf2b420b64f08962e84bb06
niwenhao <niwenhao@gmail.com>
2018-02-09 20:12:17 +0900

    up

---
+++ admin-client/src/ts/common/entities.tsx
@@ -0,0 +1,157 @@
+import * as Backbone from 'backbone'
+
+export interface User {
+  userID: string
+  userName: string
+  password: string
+}
+
+export class UserModel extends Backbone.Model implements User {
+  constructor(attrs?: User, options?: any) {
+    super(attrs || {}, options)
+  }
+  get userID() : string {
+    return this.get('userID')
+  }
+
+  set userID(v: string) {
+    this.set('userID', v)
+  }
+
+  get userName(): string {
+    return this.get('userName')
+  }
+
+  set userName(v: string) {
+    this.set('userName', v)
+  }
+
+  get password(): string {
+    return this.get('password')
+  }
+  set password(v: string) {
+    this.set('password', v)
+  }
+}
+
+export class UserCollection extends Backbone.Collection<UserModel> {
+  url = "/api/users"
+
+  model = UserModel
+}
+
+export class ClientModel extends Backbone.Model {
+  url = () => "/api/adminclient"
+
+  get clientKey(): string {
+    return this.get('clientKey')
+  }
+  set clientKey(v: string) {
+    this.set('clientKey', v)
+  }
+  get clientName(): string {
+    return this.get('clientName')
+  }
+  set clientName(v: string) {
+    this.set('clientName', v)
+  }
+}
+
+export interface Api {
+  apiPath: string
+  apiName: string
+  conditionJson: string
+  responseJson: string
+}
+
+export class ApiModel extends Backbone.Model implements Api {
+  constructor(attr?: any, options?:any) {
+    super(attr || {}, options)
+  }
+
+  get apiPath(): string {
+    return this.get('apiPath')
+  }
+
+  set apiPath(v: string) {
+    this.set('apiPath', v)
+  }
+
+  get apiName(): string {
+    return this.get('apiName')
+  }
+
+  set apiName(v: string) {
+    this.set('apiName', v)
+  }
+
+  get conditionJson(): string {
+    return this.get('conditionJson')
+  }
+
+  set conditionJson(v: string) {
+    this.set('conditionJson', v)
+  }
+
+  get responseJson(): string {
+    return this.get('responseJson')
+  }
+
+  set responseJson(v: string) {
+    this.set('responseJson', v)
+  }
+}
+
+export class ApiCollection extends Backbone.Collection<ApiModel> {
+  constructor(readonly idOfUser: string) {
+    super()
+  }
+
+  url = () => `/api/users/${this.idOfUser}/apis`
+  model = ApiModel
+}
+
+export interface History {
+  accessTime: number
+  requestJson: string
+  responseJson: string
+}
+
+export class HistoryModel extends Backbone.Model implements History {
+  constructor(attrs?: any, options?:any) {
+    super(attrs, options)
+  }
+
+  get accessTime(): number {
+    return this.get('accessTime')
+  }
+
+  set accessTime(v: number) {
+    this.set('accessTime', v)
+  }
+
+  get requestJson(): string {
+    return this.get('requestJson')
+  }
+
+  set requestJson(v: string) {
+    this.set('requestJson', v)
+  }
+
+  get responseJson(): string {
+    return this.get('responseJson')
+  }
+
+  set responseJson(v: string) {
+    this.set('responseJson', v)
+  }
+}
+
+export class HistoryCollection extends Backbone.Collection<HistoryModel> {
+  constructor(readonly apiId: string) {
+    super()
+  }
+
+  url = () => `/api/hist/api/${this.apiId}`
+  model = HistoryModel
+}
